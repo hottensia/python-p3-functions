782ace24cd61e13413998d7242a00ffb8b05e2de
Hottensiah Wanjiku <w.hottensiah96@gmail.com>
2024-07-02 20:24:57 +0300

    First Commit

--- lib/functions.py
+++ lib/functions.py
@@ -4,11 +4,11 @@ def greet_programmer():
 
 
 def greet(name):
-    print (f"Hello ,{name}!")
+    print (f"Hello, {name}!")
 
 
 def greet_with_default(name="programmer"):
-    print (f"Hello ,{name}!")
+    print (f"Hello, {name}!")
     
 
 def add(num1, num2):
